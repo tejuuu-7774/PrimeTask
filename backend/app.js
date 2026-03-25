07c6b0ef5c13e1ce1dfcfa3d3950aedc77052bc1
Palwai Tejaswini <palwaitejaswini7777@gmail.com>
2026-03-25 12:48:17 +0530

    Deployed both frontend and backend and fixed minor issues.

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,10 @@ import taskRoutes from "./routes/taskRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
-// app.use(cors({
-//   origin: "https://your-frontend.vercel.app",
-//   credentials: true
-// }));
-app.use(cors())
+app.use(cors({
+  origin: "https://prime-task-mu.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/test", testRoutes);
